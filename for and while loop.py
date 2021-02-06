e456438826d40666fb3387f36cb8a8889d759269
Mahendra-Moorthy <kmahen1106@2gmail.com>
2021-02-06 19:34:14 +0200

    for loop

---
+++ for and while loop.py
@@ -0,0 +1,57 @@
+"""x = (1,2,3,4,5)
+for y in x:
+    print(y)
+#-----------------------------------------------------------
+a = 1
+b = 5
+while a<=5:
+    print("%d * %d = %d" %(a, b, a*b))
+    a+=1
+#-----------------------------------------------------------
+
+a = 1
+while a<=10:
+    print(a, end='*')
+    a+=1
+#-----------------------------------------------------------
+
+for a in range (10):
+    print(a)
+
+#-----------------------------------------------------------
+
+a = 12
+for b in range(1,11):
+    print("%d * %d = %d" %(a,b,a*b))
+
+#-----------------------------------
+
+a = int(input("enter the value ="))
+b = int(input("enter the value ="))
+if a >=10|b>=10:
+    a+=5
+    b+=5
+    print("the bonus value is = ", a)
+if b >=10:
+    b+=5
+    print("the bonus value is = ", b)
+
+
+#------------------------------------
+
+for x in range(1,11):
+    print("The square root of the number %d is = %d" %(x,x*x))
+
+#----------------------
+
+a = int(input("Enter a = "))
+b = int(input("Enter b = "))
+c = int(input("Enter c = "))
+for x in range(a,b,1):
+    if x%c==0:
+        print(x)"""
+
+
+
+
+
